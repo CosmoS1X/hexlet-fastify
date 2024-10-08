947f562423b4868845ba337e80c1bdfd7a4e574a
Konstantin Korovin <cosmo.stan@yandex.com>
2024-10-08 19:46:48 +0300

    feat: add routes for '/users'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import fastify from 'fastify';
 
-const app = fastify();
-const port = 3000;
+export default () => {
+  const app = fastify();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
 
-app.listen({ port }, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  app.get('/users', (req, res) => {
+    res.send('GET /users');
+  });
+
+  app.post('/users', (req, res) => {
+    res.send('POST /users');
+  });
+
+  return app;
+};
