7a22b96ab4923dc689519bdf314ad61a30defc35
Konstantin Korovin <cosmo.stan@yandex.com>
2024-10-09 22:29:52 +0300

    feat: add funcs for an id generation an crypt passwords

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,15 @@
 import { faker } from '@faker-js/faker';
+import CryptoJS from 'crypto-js';
+
+export const crypto = (password) => CryptoJS.SHA256(password);
+
+export const generateId = () => faker.string.uuid();
 
 const createRandomUser = () => ({
   userId: faker.string.uuid(),
-  username: faker.internet.userName(),
-  email: faker.internet.email(),
+  username: faker.internet.userName().trim(),
+  email: faker.internet.email().toLowerCase().trim(),
+  password: crypto(faker.internet.password()),
 });
 
 export default () => {
