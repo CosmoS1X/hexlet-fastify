3a16dc586336b8ca8c13da1849e81254408f133f
Konstantin Korovin <cosmo.stan@yandex.com>
2024-10-17 18:34:16 +0300

    feat: change database to remote db on 'render.com'

--- src/db.js
+++ src/db.js
@@ -1,9 +1,13 @@
 import postgres from 'postgres';
+import 'dotenv/config';
 
 const sql = postgres({
-  user: 'cosmo',
-  database: 'hexlet-fastify',
-  host: 'localhost',
+  host: 'dpg-cs811e3qf0us738ikg3g-a.frankfurt-postgres.render.com',
+  port: 5432,
+  database: 'hexlet_fastify',
+  username: 'cosmo',
+  password: process.env.PGPASSWORD,
+  ssl: true,
 });
 
 export default sql;
